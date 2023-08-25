88e4cb8abe7bf748454f002c7bca6e2c99cc9eca
HrishikShaji <hrishikminishaji@gmail.com>
2023-08-25 20:15:42 +0530

    home done

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,24 @@
-const HomePage = () => {
-  return <div>Home</div>;
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/Billboard";
+import ProductList from "@/components/ProductList";
+import Container from "@/components/ui/Container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("64e883a03bab78af12142a6b");
+  const products = await getProducts({ isFeatured: true });
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
